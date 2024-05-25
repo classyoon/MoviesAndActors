0ddb06e33bdad83427061dba3c8a25403199ea7e
classyoon <96508736+classyoon@users.noreply.github.com>
2024-05-25 13:58:50 -0400

    Initial Commit

---
+++ MoviesAndActors/MoviesAndActorsApp.swift
@@ -0,0 +1,32 @@
+//
+//  MoviesAndActorsApp.swift
+//  MoviesAndActors
+//
+//  Created by Conner Yoon on 5/25/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MoviesAndActorsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
