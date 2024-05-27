cd303302a0bb019922f1195514d375da1d3b3617
classyoon <96508736+classyoon@users.noreply.github.com>
2024-05-27 15:43:40 -0400

    We got colors into lists and sheets to make new actors

--- MoviesAndActors/ActorListView.swift
+++ MoviesAndActors/ActorListView.swift
@@ -10,12 +10,18 @@ import SwiftData
 struct ActorListView: View {
     @Environment (\.modelContext) var modelContext
     @Query private var actors : [Actor]
+    @State private var isShowingSheet : Bool = false
+    @State private var newActor : Actor?
     var body: some View {
         NavigationStack{
             List {
                 ForEach(actors){ actor in
                     NavigationLink {
-                        ActorEditView(actor: actor)
+                        ActorEditView(actor: actor, save: {actor in
+                            try? modelContext.save()
+                        }, delete: {actor in
+                            modelContext.delete(actor)
+                        })
                     } label: {
                         Text(actor.name)
                     }
@@ -25,20 +31,58 @@ struct ActorListView: View {
             }
             .listStyle(.plain)
             .navigationTitle("Actors")
-                .navigationTitle("Home view")
-                .toolbarBackground(.yellow,
-                                   for: .navigationBar)
-                .toolbarBackground(.visible,
-                                   for: .navigationBar)
+            .toolbar{
+                Button(action: {
+                    newActor = Actor()
+                    modelContext.insert(newActor!)
+                }, label: {
+                    Text("Show Sheet")
+                })
+            }
+            .sheet(item: $newActor, content: { actor in
+                NavigationStack{
+                    ZStack{
+                        Color.red
+                        ActorEditView(actor: actor, save: {_ in}, delete: {actor in
+                            modelContext.delete(actor)
+                            
+                        }).navigationTitle("Add Actor").padding()
+                    }
+                }
+            })
+            .navigationTitle("Home view")
+            .toolbarBackground(.yellow,
+                               for: .navigationBar)
+            .toolbarBackground(.visible,
+                               for: .navigationBar)
         }
     }
 }
 struct ActorEditView : View {
     @Bindable var actor : Actor
+    var save : (Actor)->()
+    var delete : (Actor)->()
+    @Environment(\.dismiss) var dismiss
     var body: some View {
         Form {
-            TextField("", text: $actor.name)
-        }
+            
+            TextField("Type Actor Name", text: $actor.name)
+            
+                Button(action: {
+                    save(actor)
+                    dismiss()
+                }, label: {
+                    Text("Save")
+                })
+                Button(action: {
+                    delete(actor)
+                    dismiss()
+                }, label: {
+                    Text("Delete")
+                })
+            
+            
+        }.background(Color.yellow).scrollContentBackground(.hidden)
     }
 }
 #Preview {
